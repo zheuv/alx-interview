7eda2a774e17897ed44237abb373b7ca90c5e4c7
mouhcine wildane <muhssenwildane@gmail.com>
2024-08-01 02:37:38 +0100

    is all good?

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""
+Module for canUnlockAll function
+"""
+
+
+def canUnlockAll(boxes):
+    """
+    Determines if all boxes can be unlocked.
+
+    Each box may contain keys to other boxes.
+    The function checks if all boxes can be
+    unlocked starting from the first box (index 0).
+
+    Parameters:
+    boxes (list of lists of int): A list where
+    each element is a list of integers representing keys.
+
+    Returns:
+    bool: True if all boxes can be unlocked, False otherwise.
+    """
+    keys = set()
+    l = len(boxes)
+    unlocked = 1
+
+    for i in range(l):
+        alreadyUpdatedI = False
+        alreadyUpdatedJ = False
+        j = l - (i + 1)
+        if len(keys) != 0:
+            if boxes[i] != "unlocked":
+                if i in keys:
+                    keys.update(set(boxes[i]))
+                    alreadyUpdatedI = True
+                    unlocked += 1
+                    boxes[i] = "unlocked"
+            if (boxes[j] != "unlocked") and (j != 0):
+                if j in keys:
+                    keys.update(set(boxes[j]))
+                    alreadyUpdatedJ = True
+                    unlocked += 1
+                    boxes[j] = "unlocked"
+        if alreadyUpdatedI is False:
+            keys.update(set(boxes[i]))
+        if alreadyUpdatedJ is False:
+            keys.update(set(boxes[j]))
+    if unlocked == l:
+        return True
+    return False
