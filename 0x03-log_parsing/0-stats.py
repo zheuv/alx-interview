699903e5e975b602eaaafa7d02d4eb74a6ad6f54
Mouhcine Wildane <117749214+zheuv@users.noreply.github.com>
2024-08-16 01:22:30 +0100

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -22,6 +22,7 @@ status_counts = {
 
 line_count = 0
 
+
 def print_stats():
     """Prints the computed metrics."""
     print(f"File size: {total_size}")
@@ -29,16 +30,20 @@ def print_stats():
         if status_counts[status_code] > 0:
             print(f"{status_code}: {status_counts[status_code]}")
 
+
 def signal_handler(sig, frame):
     """Handles keyboard interruption (CTRL + C)."""
     print_stats()
     sys.exit(0)
 
+
 signal.signal(signal.SIGINT, signal_handler)
+
 
 def checkFileSize(size):
     """Checks if size can be converted to an integer."""
     return can_be_converted_to_int(size)
+
 
 def checkDate(date):
     """Validates if the date is in the correct format."""
@@ -47,20 +52,24 @@ def checkDate(date):
         return True
     return False
 
+
 def checkGet(get):
     """Checks if the GET request matches the expected format."""
     sentence = ['"GET', '/projects/260', 'HTTP/1.1"']
     return get == sentence
+
 
 def checkStatusCode(code):
     """Validates if the status code is one of the allowed values."""
     possibleCodes = [200, 301, 400, 401, 403, 404, 405, 500]
     return can_be_converted_to_int(code) and int(code) in possibleCodes
 
+
 def checkIp(ip):
     """Checks if the IP address is valid."""
     ip = ip.split('.')
     return len(ip) == 4 and all(x is True for x in [can_be_converted_to_int(x) for x in ip])
+
 
 def can_be_interpreted_as_date(date_string):
     """Checks if the string can be interpreted as a date."""
@@ -70,6 +79,7 @@ def can_be_interpreted_as_date(date_string):
     except ValueError:
         return False
 
+
 def can_be_converted_to_int(element):
     """Checks if the element can be converted to an integer."""
     try:
@@ -77,6 +87,7 @@ def can_be_converted_to_int(element):
         return True
     except ValueError:
         return False
+
 
 def checkInput(line):
     """Validates and processes a single input line."""
@@ -89,6 +100,7 @@ def checkInput(line):
                 total_size += int(line[8])
                 return True
     return False
+
 
 try:
     for line in sys.stdin:
@@ -105,5 +117,5 @@ except KeyboardInterrupt:
     print_stats()
     sys.exit(0)
 
-print_stats()
 
+print_stats()
